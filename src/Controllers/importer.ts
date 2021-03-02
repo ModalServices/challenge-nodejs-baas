2857e3df58cad96b5d1817410da398c81e21d6b8
Fabricio Gonzalez França Ribeiro <fabriciogonzalezfr@gmail.com>
2021-03-01 23:39:53 -0300

    System Controllers work in progress

--- src/Controllers/importer.ts
+++
@@ -1,5 +0,0 @@
-export { accountRouter } from './Routes/AccountRouter';
-
-export { P2PRouter } from './Routes/P2PRouter';
-
-export { UserRouter } from './Routes/UserRouter';
---
+++ src/Controllers/userController.ts
@@ -0,0 +1,18 @@
+import '@Utils/module-alias';
+import { Request, Response, NextFunction } from 'express';
+
+export class userController {
+  
+  listAllUsers = (Request: Request, Response: Response, NextFunction: NextFunction) => {
+
+  };
+
+  listUserDetails = (Request: Request, Response: Response, NextFunction: NextFunction) => {
+
+  }
+
+  saveUser = (Request:Request, Response: Response, NextFunction: NextFunction) => {
+
+  }
+
+} 
