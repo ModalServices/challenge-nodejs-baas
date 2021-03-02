7b92787ceb58ca9d1d2c556f3b79d06d1ac78627
Fabricio Gonzalez França Ribeiro <fabriciogonzalezfr@gmail.com>
2021-03-01 23:36:07 -0300

    Users System Model Ready

---
+++ src/Entities/Users.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+  name: string;
+  lastName: string;
+  birthDate: string;
+  maritalStatus: string;
+  address: string;
+  houseNumber: string;
+  complement: string;
+  profission: string
+  minimumIncome: string;
+}
+
